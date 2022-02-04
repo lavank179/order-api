49a97329adfbca037052a416f3b66d75b3beba07
lavank179 <chlavankumar179@gmail.com>
2022-02-04 13:15:00 +0530

    removing unnecesary elements

--- orderCalculate.js
+++ orderCalculate.js
@@ -57,7 +57,7 @@ function calculateDelivery(distance) {
         delivery_cost = delivery_cost_slab[1];
     } else if (distance > 20 && distance <= 50) {
         delivery_cost = delivery_cost_slab[2];
-    } else if(distance > 50 && distance <= 500) {
+    } else if (distance > 50 && distance <= 500) {
         delivery_cost = delivery_cost_slab[3];
     }
 
@@ -92,51 +92,54 @@ const OrderTotal = (order) => {
         [error, successCheck] = await checkTypeFormat(order, error, successCheck);
 
 
-        // iterate to the items list and multiply and add the every item price value with their quantities.
-        for (const el of order.order_items) {
-            itemTotal += el.price * el.quantity;
-        }
-
-
-        // calling delivery fee calculation method.
-        deliveryFee = await calculateDelivery(order.distance);
-
-        // adding the total items and delivery cost values.
-        orderValue = itemTotal + deliveryFee;
-
-
-
-        // checking if offer is availabe or not.
-        if ("offer" in order) {
-
-            // If it is available, check whether it is FLAT or DELIVERY and take it as discount amount.
-            if (order.offer.offer_type === "FLAT") {
-                discountAmount = order.offer.offer_val;
-            } else if (order.offer.offer_type === "DELIVERY") {
-                discountAmount = deliveryFee;
-            } else {
-                discountAmount = 0;
-            }
-        }
-
-
-        // maximum discount should be order value.
-        // If it exceeds the order value, reduce the discount Amount equals to the Order Value. (i.e Final Cost = 0)
-        // If it is less than order value, reduce the discount Amount from the Order Value.
-        // If there is no Offer, the  Order value will be as it is before.
-        if (discountAmount > orderValue) {
-            orderValue = 0;
-        } else {
-            orderValue = orderValue - discountAmount;
-        }
-
-
-        // Returning the final calculated 'OrderValue' Cost in the JSON format.
         if (successCheck) {
+            // iterate to the items list and multiply and add the every item price value with their quantities.
+            for (const el of order.order_items) {
+                itemTotal += el.price * el.quantity;
+            }
+
+
+            // calling delivery fee calculation method.
+            deliveryFee = await calculateDelivery(order.distance);
+
+            // adding the total items and delivery cost values.
+            orderValue = itemTotal + deliveryFee;
+
+
+
+            // checking if offer is availabe or not.
+            if ("offer" in order) {
+
+                // If it is available, check whether it is FLAT or DELIVERY and take it as discount amount.
+                if (order.offer.offer_type === "FLAT") {
+                    discountAmount = order.offer.offer_val;
+                } else if (order.offer.offer_type === "DELIVERY") {
+                    discountAmount = deliveryFee;
+                } else {
+                    discountAmount = 0;
+                }
+            }
+
+
+            // maximum discount should be order value.
+            // If it exceeds the order value, reduce the discount Amount equals to the Order Value. (i.e Final Cost = 0)
+            // If it is less than order value, reduce the discount Amount from the Order Value.
+            // If there is no Offer, the  Order value will be as it is before.
+            if (discountAmount > orderValue) {
+                orderValue = 0;
+            } else {
+                orderValue = orderValue - discountAmount;
+            }
+
+
+
+            // Returning the final calculated 'OrderValue' Cost in the JSON format.
             resolve({ 'order_total': orderValue });
         } else {
             reject(error);
         }
+
+
     });
 };
 
